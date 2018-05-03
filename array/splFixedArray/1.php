576f42ab2d9b8fb8001f65f31ddbc07b27dbb540
Sultan imam muttaqin <im.sultaan05@gmail.com>
2018-05-03 12:44:16 +0700

    repair

--- array/splFixedArray/1.php
+++ array/splFixedArray/1.php
@@ -1,6 +1,6 @@
 <?php
-$array	=	new	SplFixedArray(10);
-for	($i	= 0; $i	< 10;	$i++)
-	$array[$i]	=	$i;
-for	($i	= 0; $i	< 10;	$i++)
+$array = new SplFixedArray(10);
+for	($i	= 0; $i	< 10; $i++)
+	$array[$i] = $i;
+for	($i	= 0; $i	< 10; $i++)
 	echo $array[$i]	. "\n";
--- array/splFixedArray/3.php
+++ array/splFixedArray/3.php
@@ -1,8 +1,8 @@
 <?php 
-$items	=	5;	
-$array	=	new	SplFixedArray($items);
-for	($i	=	0;	$i	<	$items;	$i++){
-	$array[$i]	=	$i	*	10;	
-}	
-$newArray	=	$array->toArray();
+$items = 5;	
+$array = new SplFixedArray($items);
+for	($i	= 0; $i < $items; $i++){
+	$array[$i] = $i * 10;	
+}
+$newArray =	$array->toArray();
 print_r($newArray);
