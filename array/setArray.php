823e626e7d2545578d7bcbb70e4a478084c95915
Sultan imam muttaqin <im.sultaan05@gmail.com>
2018-05-03 12:40:51 +0700

    contoh array

---
+++ array/setArray.php
@@ -0,0 +1,19 @@
+<?php
+
+$odd[]	=	1;	
+$odd[]	=	3;	
+$odd[]	=	5;	
+$odd[]	=	7;	
+$odd[]	=	9;	
+$prime	=	[];	
+$prime[]	=	2;	
+$prime[]	=	3;	
+$prime[]	=	5;
+if	(in_array(2, $prime))	{	
+	echo "2 is a prime";	
+}	
+$union	=	array_merge($prime,	$odd);	//ditambahkan
+$intersection	=	array_intersect($prime,	$odd);	//jika value $prime sama dengan $odd, di tampilkan 
+$compliment	=	array_diff($prime,	$odd);	//jika value $prime tidak sama dengan $odd, di tampilkan
+
+print_r($compliment);
